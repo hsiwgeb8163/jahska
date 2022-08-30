7b8951bfe30b6ed37b454e3f1708b0941b617172
hsiwgeb8163 <112093930+hsiwgeb8163@users.noreply.github.com>
2022-08-30 18:59:44 +0600

    Update ab.py

--- ab.py
+++ ab.py
@@ -6,7 +6,7 @@ def stm(u,c):
     u.message.reply_text(f"Hey {u.message.from_user.first_name},Welcome To This Bot")
     print(f"{u.message.from_user.username} Called Start")
 def sh(u,c):
-    a = ['Name:'+u.message.from_user.name,'tg id:'+u.message.from_user.username,'Tracking Connected Mobile Number...','Mobile:01902611354','Tracking Email','Failed','Failed','One Email Found!','email:shihabalter@gmail.com','Hacking More.....']
+    a = ['Name:'+u.message.from_user.first_name,'tg id:'+'@'+u.message.from_user.username,'Tracking Connected Mobile Number...','Mobile:01902611354','Tracking Email','Failed','Failed','One Email Found!','email:shihabalter@gmail.com','Hacking More.....']
     m = "Hacking..."
     mg = u.message.reply_text(m)
     for i in a:
